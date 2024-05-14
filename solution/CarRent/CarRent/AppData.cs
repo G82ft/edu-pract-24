1f073f95ec138c8187952d6c31d868df5c87274d
G4m3-80ft <leonidkhvan123@gmail.com>
2024-05-14 21:29:48 +0300

    Init solution, add authentication

---
+++ solution/CarRent/CarRent/AppData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CarRent
+{
+    internal class AppData
+    {
+        public static Frame MainFrame;
+        public static CarRentEntities Model;
+    }
+}
---
+++ solution/CarRent/CarRent/Pages/Auth.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarRent.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            if(AppData.Model.Users.Where(x => x.Login == login.Text && x.Password == pwd.Password).Any())
+            {
+                MessageBox.Show($"Добро пожаловать, {login.Text}!");
+            }
+        }
+
+        private void SignUp(object sender, RoutedEventArgs e)
+        {
+            string loginTxt = login.Text;
+
+            if (AppData.Model.Users.Where(x => x.Login == loginTxt).Any())
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует!");
+                return;
+            }
+            string password = pwd.Password;
+            Users user = new Users()
+            {
+                Login = loginTxt,
+                Password = password,
+                Role = 1
+            };
+            AppData.Model.Users.Add(
+                user
+            );
+            AppData.Model.SaveChanges();
+            MessageBox.Show("Регистрация прошла успешно!");
+        }
+    }
+}
