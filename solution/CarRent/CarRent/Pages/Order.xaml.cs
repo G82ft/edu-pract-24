516b4f50739be409247171f8f6ba7b9ad6fd4b3e
G4m3-80ft <leonidkhvan123@gmail.com>
2024-05-21 17:55:27 +0300

    Add date choosing

---
+++ solution/CarRent/CarRent/Pages/Order.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarRent.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Order.xaml
+    /// </summary>
+    public partial class Order : Page
+    {
+        public CarRent.Orders orders { get; set; }
+        public Order(CarRent.Orders order)
+        {
+            InitializeComponent();
+            orders = order;
+            DataContext = orders;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            AppData.Model.SaveChanges();
+            AppData.MainFrame.GoBack();
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            AppData.Model.Orders.Remove(orders);
+            Save(sender, e);
+        }
+    }
+}
