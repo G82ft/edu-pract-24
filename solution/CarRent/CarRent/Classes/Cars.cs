943f03f67693f9cd8a049b523df9f65bbff8e44e
G4m3-80ft <leonidkhvan123@gmail.com>
2024-05-25 17:28:38 +0300

    Add States table

---
+++ solution/CarRent/CarRent/Classes/Cars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent
+{
+    public partial class Cars
+    {
+        public string ImagePath
+        {
+            get
+            {
+                if (this.Image == null || !File.Exists($"..\\..\\Images\\{Image}"))
+                {
+                    Console.WriteLine(Directory.GetCurrentDirectory());
+                    return $"..\\Images\\def.jpg";
+                }
+                return $"..\\Images\\{Image}";
+            }
+        }
+    }
+}
